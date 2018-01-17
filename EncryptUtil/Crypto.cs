9fb5ba2a6d62313d6a22a4c007d590f2d22e4532
Arthur Cardoso <cardosovha@gmail.com>
2018-01-17 16:03:29 -0400

    Encryption key gen created

---
+++ EncryptUtil/Crypto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace EncryptUtil
+{
+    public class EncryptUtil
+    {
+        private static string EncryptionKey;
+
+        /// <summary>
+        /// Method that generates a random encryption key
+        /// </summary>
+        /// <param name="length">Key length. Min value: 6</param>
+        public void GenerateKey(int length)
+        {
+            length = Math.Max(length, 6);
+            byte[] buffer = new byte[length];
+            RandomNumberGenerator.Create().GetBytes(buffer);
+
+            EncryptionKey = Convert.ToBase64String(buffer)
+                .Replace("/", "").Replace("+", "").Replace("=", "")
+                .Remove(length);
+        }
+
+    }
+}
